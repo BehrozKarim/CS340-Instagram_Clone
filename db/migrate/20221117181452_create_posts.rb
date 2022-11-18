3bc68015d7a17a3fa8b9202fcc72b87e1a988fc3
Behroz Karim <behrozbuzdar@gmail.com>
2022-11-18 21:39:27 +0500

    Removed AWS and Post errors
    on create post, active = true,
    aws region
    added username, first and last name columns to accounts table

--- db/migrate/20221117181452_create_posts.rb
+++ db/migrate/20221117181452_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :image
       t.references :account
-      t.boolean :active
+      t.boolean :active, default: true
       t.timestamps
     end
   end
