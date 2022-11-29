f0fe8ddb4bb7ceaff0d1527b3631dee691547426
Behroz Karim <behrozbuzdar@gmail.com>
2022-11-30 01:29:40 +0500

    View Profile, Update Account Infor
    Following Changes:
    -Added a view for profile
    -Added update account information functionality
    -Added Delete account functionality
    -Added a field for profile image
    -Updated Routes and Other relevant files

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,5 +5,21 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  mount_uploader :image, ImageUploader
+
   has_many :posts, dependent: :destroy
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def total_followers
+    # self.followers.count
+    0
+  end
+
+  def total_following
+    # self.following.count
+    0
+  end
 end
