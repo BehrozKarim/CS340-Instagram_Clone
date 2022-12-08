1d0c15b4e9fbc7fb873c0c271d0dad833643131a
Behroz Karim <behrozbuzdar@gmail.com>
2022-12-08 13:01:34 +0500

    Added UI for Likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,17 +4,21 @@ class LikesController < ApplicationController
     def save_like
         @like = Like.new(post_id: params(:post_id), account_id: current_account.id)
         @post_id = params[:post_id]
-            respond_to do |format|
-                format.js {
-                    if @like.save
-                        @success = true
-                    else
-                        @success = false
-                    end 
-                    
-                    render "post/like"
 
-                }
+        existing_like = Like.where(post_id: params[:post_id], account_id: current_account.id)
+        respond_to do |format|
+            format.js {
+                if existing_like.any?
+                    existing_like.first.destroy
+                    @success = false 
+                elsif @like.save
+                    @success = true
+                else
+                    @success = false
+                end 
+                
+                render "post/like"
+            }
     end
     
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ApplicationRecord
     belongs_to :account
     belongs_to :post
+    validates_uniqueness_of :post_id, scope: :account_id
 end
