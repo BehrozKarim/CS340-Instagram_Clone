ff2e9fa77da986ad1f7f29398540e8b2c8738e4d
Behroz Karim <behrozbuzdar@gmail.com>
2022-12-09 18:36:08 +0500

    Enhanced

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,8 @@ class CommentsController < ApplicationController
         @comment.account_id = current_account.id if account_signed_in?
         
         if @comment.save
-            redirect_to dashboard_path, flash: { success: "Comment created successfully" }
+            return_url = params[:comment][:return_to].present? ? post_path(@comment.post.id) : dashboard_path
+            redirect_to return_url, flash: { success: "Comment created successfully" }
         else
             redirect_to dashboard_path, flash: { danger: "Comment not created" }
         end
